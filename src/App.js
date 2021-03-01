ce7c2611101847ebf45e5aab14268bc5972b8508
ysonbrian <znlsakxls@gmail.com>
2021-03-01 19:04:07 +0900

    Modal updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/Searchbar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -16,6 +20,10 @@ const App = () => {
 			<SearchBar />
 			<div className='container'>
 				<AddBtn />
+				<AddLogModal />
+				<EditLogModal />
+				<AddTechModal />
+				<TechListModal />
 				<Logs />
 			</div>
 		</Fragment>
