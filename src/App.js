9de3b0f5bb13058a991619f4e83e0d2796189a1a
ysonbrian <znlsakxls@gmail.com>
2021-03-02 16:36:01 +0900

    adding redux && reducers

--- src/App.js
+++ src/App.js
@@ -6,28 +6,32 @@ import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 const App = () => {
-	useEffect(() => {
-		// Init Materialize JS
-		M.AutoInit();
-	});
-	return (
-		<Fragment>
-			<SearchBar />
-			<div className='container'>
-				<AddBtn />
-				<AddLogModal />
-				<EditLogModal />
-				<AddTechModal />
-				<TechListModal />
-				<Logs />
-			</div>
-		</Fragment>
-	);
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
+  );
 };
 
 export default App;
--- src/components/layout/Searchbar.js
+++ src/components/layout/Searchbar.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 const Searchbar = () => {
-	return (
-		<nav style={{ marginBottom: '30px' }} className='blue'>
-			<div className='nav-wrapper'>
-				<form>
-					<div className='input-field'>
-						<input id='search' type='search' required />
-						<label className='label-icon' htmlFor='search'>
-							<i className='material-icons'>search</i>
-						</label>
-						<i className='material-icons'>close</i>
-					</div>
-				</form>
-			</div>
-		</nav>
-	);
+  return (
+    <nav style={{ marginBottom: '30px' }} className='blue'>
+      <div className='nav-wrapper'>
+        <form>
+          <div className='input-field'>
+            <input id='search' type='search' required />
+            <label className='label-icon' htmlFor='search'>
+              <i className='material-icons'>search</i>
+            </label>
+            <i className='material-icons'>close</i>
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
 };
 
 export default Searchbar;
